f1fda62b1847cac32092dad7d565fe1a6d23dc8c
Louis31423142 <Louiswood3142@gmail.com>
2025-07-16 10:25:21 +0100

    added address argument

--- set_credentials.py
+++ set_credentials.py
@@ -4,21 +4,26 @@ import argparse
 
 from bleak import BleakClient
 
-ADDRESS = "2C:CF:67:BE:04:21"
-
 SSID_CHARACTERISTIC = "b1829813-e8ec-4621-b9b5-6c1be43fe223"
 PASSWORD_CHARACTERISTIC = "410f5077-9e81-4f3b-b888-bf435174fa58"
 
-parser=argparse.ArgumentParser(description="ssid and password parser")
+#Add arguments from terminal with python3 set_credentials.py ssid password address
+parser=argparse.ArgumentParser(description="ssid, password and address parser")
 parser.add_argument("ssid")
 parser.add_argument("password")
+parser.add_argument("address")
 args = parser.parse_args()
 
 ssid = args.ssid
 password = args.password
+address = args.address
+
+print("submitted ssid: ", ssid)
+print("submitted password: ", password)
+print("submitted address: ", address)
 
 async def main(ssid, password):
-    async with BleakClient(ADDRESS) as client:
+    async with BleakClient(address) as client:
         print(f"Connected: {client.is_connected}")
 
         print("Writing SSID...")
